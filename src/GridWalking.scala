a7825ec3d6f2fa516d25fb549233f38979e33d83
Charley Lewittes <ctlewitt@gmail.com>
2016-06-07 17:53:35 -0400

    completed GridWalking functionality.  Still has some bugs.

--- src/GridWalking.scala
+++ src/GridWalking.scala
@@ -9,12 +9,16 @@ object GridWalking {
     //go through each test
     for(_ <- 0 until numTests){
       val numDims = sc.nextInt()
-      val numSteps = sc.nextInt()
+      val numMoves = sc.nextInt()
       //get starting position
       val start_pos = fillInList(List(), numDims, sc)
       //get dimension of each row
       val dimOfRow = fillInList(List(), numDims, sc)
-
+      val allPositions: List[List[Int]] = generateAllPositions(dimOfRow)
+      val initialMap: Map[List[Int], BigInt] = Map() + (start_pos -> 1)
+      val finalMap = makeAllMoves(initialMap, numMoves, allPositions)
+      val totalPossibleMoves = finalMap.values.sum
+      println(totalPossibleMoves)
     }
 
 
@@ -31,5 +35,65 @@ object GridWalking {
     }
   }
 
+  def makeAllMoves(possibleMovesToDate: Map[List[Int], BigInt], numMovesLeft: Int, allPositions: List[List[Int]]) : Map[List[Int], BigInt] = {
+    if(numMovesLeft > 0){
+      makeAllMoves(makeOneMove(possibleMovesToDate, allPositions), numMovesLeft-1, allPositions)
+    }
+    else{
+      possibleMovesToDate
 
+    }
+
+  }
+
+  def makeOneMove(possibleMovesToDate: Map[List[Int], BigInt], allPositions: List[List[Int]]): Map[List[Int], BigInt] ={
+    var updatedPossibleMoves: Map[List[Int], BigInt] = Map()
+    for (pos <- allPositions){
+      updatedPossibleMoves = updatedPossibleMoves.updated(pos, makeMiniMove(pos,possibleMovesToDate)) //MIGHT NEED TO SAY UPDATEDPOSSIBLEMOVES = THIS
+    }
+    updatedPossibleMoves
+  }
+
+
+  //helper function: gets total number of moves options that could lead to this position
+  def makeMiniMove(position: List[Int], possibleMovesToDate: Map[List[Int], BigInt]): BigInt ={
+    var sum:BigInt = 0
+    for (idx <- position.indices){ //try "position.indicies" instead
+      val positionPlus = position.updated(idx, position.apply(idx) +1)
+      val positionMinus = position.updated(idx, position.apply(idx) -1)
+      sum += possibleMovesToDate.getOrElse(positionPlus, 0)
+      sum += possibleMovesToDate.getOrElse(positionMinus, 0)
+    }
+    sum
+  }
+
+  //helper function: generates a list containing lists that represent the possible positions in the dimOfRow.length-dimensional board
+  def generateAllPositions(dimOfRow: List[Int]) : List[List[Int]] ={
+    if(dimOfRow.nonEmpty){
+      var updatedListOfPositions: List[List[Int]] = List()
+
+      for(pos <- 1 to dimOfRow.head) {
+        updatedListOfPositions = updatedListOfPositions :+ List(pos)
+      }
+      generateAllPositionsAux(dimOfRow.tail, updatedListOfPositions)
+    }
+    else{
+      List()
+    }
+  }
+
+  //helper function: generates a list containing lists that represent the possible positions in the dimOfRow.length-dimensional board
+  def generateAllPositionsAux(dimOfRow: List[Int], listOfPositions: List[List[Int]]) : List[List[Int]] ={
+    if(dimOfRow.nonEmpty){
+      var updatedListOfPositions: List[List[Int]] = List()
+
+      for(pos <- 1 to dimOfRow.head) {
+        updatedListOfPositions = updatedListOfPositions ::: listOfPositions.mapConserve[List[Int]](myList => myList :+ pos)
+      }
+      generateAllPositionsAux(dimOfRow.tail, updatedListOfPositions)
+    }
+    else{
+      listOfPositions
+    }
+  }
 }
